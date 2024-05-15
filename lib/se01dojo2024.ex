c684d4bd9f5def7141e4bee7b9857011df489216
NoEpicLoot <NoEpicLoot@users.noreply.github.com>
2024-05-15 22:31:03 +0200

    cleanup

--- lib/se01dojo2024.ex
+++ lib/se01dojo2024.ex
@@ -1,6 +1,8 @@
 defmodule Se01dojo2024 do
   @moduledoc """
   Documentation for `Se01dojo2024`.
+
+  excercises done as preparation for the coding dojo
   """
 
   @doc """
@@ -26,36 +28,4 @@ defmodule Se01dojo2024 do
       inner_list |> Enum.map(function)
     end)
   end
-
-  # def toTable(data, headers \\ false, index \\ false) do
-  #   tableHeader =
-  #     case headers do
-  #       # build headers
-  #       true -> "true"
-  #       false -> ""
-  #     end
-
-  #   table_content = tableHeader
-  #   "<table>" <> table_content <> "</table>"
-  # end
-
-  # defp build_table_row(row_data, cell_tag) do
-  #   row_data
-  #   |> Enum.map(fn cell -> "<#{cell_tag}>#{cell}</#{cell_tag}>" end)
-  #   |> Enum.join("")
-  #   |> Kernel.<>("</tr>")
-  #   |> Kernel.<>("<tr>")
-  # end
-
-  # defp build_table_body(data, true) do
-  #   Enum.with_index(data)
-  #   |> Enum.map(fn {row, idx} -> build_table_row([idx + 1 | row], "td") end)
-  #   |> Enum.join("")
-  # end
-
-  # defp build_table_body(data, false) do
-  #   data
-  #   |> Enum.map(fn row -> build_table_row(row, "td") end)
-  #   |> Enum.join("")
-  # end
 end
